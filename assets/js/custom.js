3d7114c6c08ef0d144122d47e0cd128327e5a950
devArman <ashavelyan@gmail.com>
2020-04-02 13:43:01 +0400

    fixings

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,12 +2,12 @@
 $(document).ready(function(){
     $('.mobile-fixed-order-box button').on('click',function () {
         $('.order-hide-desk').hide();
-        $(this).parents('.cart-total-box').find('.order-box').fadeIn();
+        $(this).parents('.cart-total-box').find('.order-box').addClass('order-box-cl');
         $('body').css('overflow','hidden');
     });
 
     $('.close-order-box').on('click',function () {
-        $(this).parents('.cart-total-box').find('.order-box').hide();
+        $(this).parents('.cart-total-box').find('.order-box').removeClass('order-box-cl');
         $(this).parents('.cart-total-box').find('.order-hide-desk').fadeIn();
         $('body').css('overflow','inherit');
     });
